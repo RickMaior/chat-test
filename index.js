0bd00290785a36a5e8b8612227434d8adf917da8
Glitch (rickmaior-chat-test) <none>
2020-01-31 02:42:24 +0000

    🏏🌵 Updated with Glitch

--- index.js
+++ index.js
@@ -1,68 +1,62 @@
-var express = require('express')
-var app =  express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+//glitch test
+
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
-var favicon = require('serve-favicon');
+var favicon = require("serve-favicon");
 
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(favicon(__dirname + "/public/images/favicon.ico"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/chat', function (req, res) {
-  res.sendFile(__dirname + '/chat.html');
+app.get("/chat", function(req, res) {
+  res.sendFile(__dirname + "/chat.html");
 });
 
-io.on('connection', function (socket) {
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.broadcast.emit("chat message", "nameinput connected ");
+  socket.emit("chat message", "you connected");
 
-  console.log('a user connected');
-  socket.broadcast.emit('chat message', "nameinput connected ");
-  socket.emit('chat message', "you connected")
-
-
-
-  socket.on('message sended', function (msg) {
+  socket.on("message sended", function(msg) {
     if (msg) {
-      console.log('message: ' + msg);
-      socket.broadcast.emit('chat message', "nameinput: " + msg);
-      socket.emit('chat message', "you: " + msg)
+      console.log("message: " + msg);
+      socket.broadcast.emit("chat message", "nameinput: " + msg);
+      socket.emit("chat message", "you: " + msg);
     }
   });
 
-  socket.on('discord message', function (msg,user) {
+  socket.on("discord message", function(msg, user) {
     if (msg) {
-      console.log("Discord:")
-      console.log('message: ' + msg);
-      console.log("user: " + user)
-      socket.broadcast.emit('chat message', `DISCORD_${user} : ${msg}`);
+      console.log("Discord:");
+      console.log("message: " + msg);
+      console.log("user: " + user);
+      socket.broadcast.emit("chat message", `DISCORD_${user} : ${msg}`);
     }
   });
 
-  socket.on('twitch message', function (msg,user) {
+  socket.on("twitch message", function(msg, user) {
     if (msg) {
-      console.log("Twitch:")
-      console.log('message: ' + msg);
-      console.log("user: " + user)
-      socket.broadcast.emit('chat message', `Twitch_${user} : ${msg}`);
+      console.log("Twitch:");
+      console.log("message: " + msg);
+      console.log("user: " + user);
+      socket.broadcast.emit("chat message", `Twitch_${user} : ${msg}`);
     }
   });
 
-  
-
-  socket.on('disconnect', function () {
-    socket.broadcast.emit('chat message', "nameinput disconnected ");
-    socket.emit('chat message', "you disconnected")
-    console.log('user disconnected');
+  socket.on("disconnect", function() {
+    socket.broadcast.emit("chat message", "nameinput disconnected ");
+    socket.emit("chat message", "you disconnected");
+    console.log("user disconnected");
   });
-
 });
 
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
